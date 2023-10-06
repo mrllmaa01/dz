18230a3e7b25686bf5528b8605beadb2ae236002
mrllmaa01 <147192466+mrllmaa01@users.noreply.github.com>
2023-10-06 23:09:20 +0500

    Задание 1

---
+++ z1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class z1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите имя: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Введите возраст: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Введите вес: ");
+        double weight = scanner.nextDouble();
+
+        System.out.println("Имя: " + name);
+        System.out.println("Возраст: " + age);
+        System.out.println("Вес: " + weight);
+    }
+}
